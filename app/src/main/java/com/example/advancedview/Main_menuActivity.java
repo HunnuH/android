a3069fb4b6798a94bc58fb2f1bb393e8f68fbc9b
HunnuH <88477795+HunnuH@users.noreply.github.com>
2021-11-04 19:25:14 +0900

    "11.04 task"

---
+++ app/src/main/java/com/example/advancedview/Main_menuActivity.java
@@ -0,0 +1,14 @@
+package com.example.advancedview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_menuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
---
+++ app/src/main/java/com/example/advancedview/Product_page_Activity.java
@@ -0,0 +1,14 @@
+package com.example.advancedview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Product_page_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_page);
+    }
+}
